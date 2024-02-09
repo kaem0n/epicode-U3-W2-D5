73e679ec78dcf2eb7cf2860f33f9903af13a54ad
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-09 17:22:31 +0100

    added dark theme

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -16,6 +16,14 @@ const MyNavBar = () => {
     setSearchInput('')
   }
 
+  const changeTheme = () => {
+    const html = document.getElementsByTagName('html')[0]
+    const theme = html.getAttribute('data-bs-theme')
+    theme === 'light'
+      ? html.setAttribute('data-bs-theme', 'dark')
+      : html.setAttribute('data-bs-theme', 'light')
+  }
+
   return (
     <Navbar collapseOnSelect expand="lg" className="fixed-top bg-body-tertiary">
       <Container>
@@ -39,12 +47,17 @@ const MyNavBar = () => {
             </Nav.Link>
           </Nav>
           <Nav>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} className="d-flex align-items-center">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-circle-half-stroke me-1"></i>
+                <Form.Switch onChange={() => changeTheme()} />
+              </div>
               <Form.Control
                 type="search"
                 placeholder="Search location"
                 value={searchInput}
                 onChange={(e) => setSearchInput(e.target.value)}
+                className="ms-3"
               />
             </Form>
           </Nav>

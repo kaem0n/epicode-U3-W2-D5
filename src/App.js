a393c1bec88020e187f7f2e850f9f8bf5ee3edf2
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-09 10:00:23 +0100

    project start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <></>
 }
 
-export default App;
+export default App
+
+// aa419503f796ec8d325d53b803ee1b7f <- API key
